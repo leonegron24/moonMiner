ba050a0c1915028702f03206fb01015cdc6dadaf
Leonardo Negron <leonegron24@gmail.com>
2024-11-08 18:13:19 -0700

    Finished! Planning to review one more time just in case

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-// Global Variables 
+// Let Variables
+    // cheese
 let cheese = 0
 let cheesePerClick = 5
+    // levels
 let knifeLevel = 1
 let shovelLevel = 1
+let elonifiedLevel = 1
+let alienLevel = 1
+    // prices
 let priceForKnifeUpgrade = 50
 let priceForGiantShovelUpgrade = 150
-const elmCheese = document.getElementById('cheese')
+let priceForElonifiedUpgrade = 500
+let priceForAlienUpgrade = 2000
+    // autoCheese
+let autoCheeseAmount = 0
+let autoCheeseIncrease = 5
+
+// Const Variables
+const elmCheese = document.getElementById('cheeseNumber')
 const elmLevelKnife = document.getElementById('levelKnife')
 const elmLevelShovel = document.getElementById('levelShovel')
+const elmLevelElonified = document.getElementById('levelElonified')
+const elmLevelAlien = document.getElementById('levelAlien')
 const elmCheesePerClick = document.getElementById('cheesePerClick')
 const elmPriceForKnifeUpgrade = document.getElementById('knifeUpgrade')
 const elmPriceForGiantShovelUpgrade = document.getElementById('shovelUpgrade')
+const elmAutoCheeseAmount = document.getElementById('autoCheese')
+const elmpriceForElonifiedUpgrade = document.getElementById('elonified')
+const elmpriceForAlienUpgrades = document.getElementById('alien')
 
 // Global Lists
 let clickUpgrades = [
@@ -75,10 +92,21 @@ function update(){
         }
     elmLevelShovel.innerHTML = shovelLevel
 
-}
+    if (!elmLevelAlien){
+        return
+    }
+    elmLevelAlien.innerHTML = alienLevel
+
+    if (!elmLevelElonified){
+        return
+    }
+    elmLevelElonified.innerHTML = elonifiedLevel
+
+    }
 
 function clickUpgrade(weapon){
     if (weapon === 'knife'){
+        console.log('clicked shovel')
         if (cheese >= 50){
         cheesePerClick += 20
         cheese -= priceForKnifeUpgrade
@@ -95,6 +123,7 @@ function clickUpgrade(weapon){
     }   
 
     if (weapon === 'shovel'){
+        console.log('clicked shovel')
         if (cheese >= 150){
             cheesePerClick += 40
             cheese -= priceForGiantShovelUpgrade
@@ -108,5 +137,57 @@ function clickUpgrade(weapon){
             }else{
                 window.alert('Not enough cheese!')
             }
-            }
+        }
+        console.log("Cheese Per Click! = " + " " + cheesePerClick)
+    }
+function autoCheese(){
+    if (!elmAutoCheeseAmount){
+        return
+    }
+    elmAutoCheeseAmount.innerHTML = '+' + autoCheeseIncrease
+    cheese += autoCheeseIncrease
+    if (!elmCheese){
+        return
+    }
+   elmCheese.innerHTML = cheese;  
+
 }
+
+function autoUpgrades(upgradeChoice){
+    if (upgradeChoice === 'Elonified'){
+        console.log('clicked Elon')
+        if (cheese >= 500){
+            autoCheeseIncrease += 10
+            cheese -= priceForElonifiedUpgrade
+            priceForElonifiedUpgrade *= 2
+            if (!elmpriceForElonifiedUpgrade){
+                return
+            }
+            elmpriceForElonifiedUpgrade.innerHTML = priceForElonifiedUpgrade
+            elonifiedLevel += 1
+            update()
+            }else{
+                window.alert('Not enough cheese!')
+            }
+        }
+    if (upgradeChoice === 'Alien' ){
+        console.log('clicked Alien')
+        if (cheese >= 2000){
+            autoCheeseIncrease += 20
+            cheese -= priceForAlienUpgrade
+            priceForAlienUpgrade *= 2
+            if (!elmpriceForAlienUpgrades){
+                return
+            }
+            elmpriceForAlienUpgrades.innerHTML = priceForAlienUpgrade
+            alienLevel += 1
+            update()
+            }else{
+                window.alert('Not enough cheese!')
+            }
+        }
+    console.log("Auto Cheese Amount! +" + " " + autoCheeseIncrease)
+}
+
+setInterval(autoCheese,3000)
+
